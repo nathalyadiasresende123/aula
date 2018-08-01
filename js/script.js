92b6ed840fb2e42a01bb164851b72426724b56a2
nathalyadiasresende123 <41390845+nathalyadiasresende123@users.noreply.github.com>
2018-08-01 14:17:13 -0300

    Subindo as aulas novamente

---
+++ js/script.js
@@ -0,0 +1,17 @@
+function mudaCor(){
+	 var corAmarela = document.getElementById('div1');
+	 corAmarela.style.background="green";
+	 corAmarela.innerHTML="<p>Estou escevendo sem escrever </p>";
+
+	}
+	function voltar(){
+		var corAmarela = document.getElementById('div1');
+		  corAmarela.style.background="yellow";
+	}
+	 
+
+
+
+
+
+
